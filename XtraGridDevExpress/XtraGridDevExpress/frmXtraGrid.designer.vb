ae418e839458a6e8203795e321f73b01ef6366cf
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-10-09 13:18:02 +0700

    Add files via upload

---
+++ XtraGridDevExpress/XtraGridDevExpress/frmXtraGrid.designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmXtraGrid
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmXtraGrid))
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.lblRecordCount = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmbDisplay = New System.Windows.Forms.ComboBox()
+        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnRefresh.Image = CType(resources.GetObject("btnRefresh.Image"), System.Drawing.Image)
+        Me.btnRefresh.Location = New System.Drawing.Point(174, 6)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(24, 24)
+        Me.btnRefresh.TabIndex = 1
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'lblRecordCount
+        '
+        Me.lblRecordCount.AutoSize = True
+        Me.lblRecordCount.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblRecordCount.Location = New System.Drawing.Point(198, 11)
+        Me.lblRecordCount.Name = "lblRecordCount"
+        Me.lblRecordCount.Size = New System.Drawing.Size(15, 13)
+        Me.lblRecordCount.TabIndex = 1
+        Me.lblRecordCount.Text = "[]"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label12.ForeColor = System.Drawing.Color.Black
+        Me.Label12.Location = New System.Drawing.Point(4, 10)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(38, 14)
+        Me.Label12.TabIndex = 60
+        Me.Label12.Text = "ค้นหา"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(43, 7)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(130, 22)
+        Me.txtSearch.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(661, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 16)
+        Me.Label1.TabIndex = 63
+        Me.Label1.Text = "การแสดงผล"
+        '
+        'cmbDisplay
+        '
+        Me.cmbDisplay.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmbDisplay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbDisplay.FormattingEnabled = True
+        Me.cmbDisplay.Location = New System.Drawing.Point(738, 5)
+        Me.cmbDisplay.Name = "cmbDisplay"
+        Me.cmbDisplay.Size = New System.Drawing.Size(131, 24)
+        Me.cmbDisplay.TabIndex = 2
+        '
+        'GridControl1
+        '
+        Me.GridControl1.Location = New System.Drawing.Point(2, 36)
+        Me.GridControl1.MainView = Me.GridView1
+        Me.GridControl1.Name = "GridControl1"
+        Me.GridControl1.Size = New System.Drawing.Size(870, 545)
+        Me.GridControl1.TabIndex = 64
+        Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
+        '
+        'GridView1
+        '
+        Me.GridView1.GridControl = Me.GridControl1
+        Me.GridView1.Name = "GridView1"
+        '
+        'frmXtraGrid
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 583)
+        Me.Controls.Add(Me.GridControl1)
+        Me.Controls.Add(Me.cmbDisplay)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.lblRecordCount)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmXtraGrid"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "XtraGrid DevExpress Sample - coDe bY: Thongkorn Tubtimkrob"
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnRefresh As System.Windows.Forms.Button
+    Friend WithEvents lblRecordCount As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmbDisplay As System.Windows.Forms.ComboBox
+    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
+End Class
---
+++ XtraGridDevExpress/XtraGridDevExpress/frmXtraGrid.vb
@@ -0,0 +1,326 @@
+﻿#Region "ABOUT"
+' / --------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gsoft.com/
+' /
+' / Purpose: Sample code to used XtraGrid DevExpress.
+' / Microsoft Visual Basic .NET (2010) + MS Access 2007+
+' /
+' / This is open source code under @Copyleft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------
+#End Region
+
+Imports System.Data.OleDb
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Columns
+Imports DevExpress.Data
+Imports DevExpress.XtraGrid
+
+Public Class frmXtraGrid
+    '// หากเป็นโปรเจคจริงๆ กลุ่มตัวแปรเหล่านี้ต้องนำไปวางไว้ใน Module 
+    Dim Conn As OleDb.OleDbConnection
+    Dim DA As New System.Data.OleDb.OleDbDataAdapter()
+    Dim Cmd As New System.Data.OleDb.OleDbCommand
+    Dim DT As New DataTable
+    Dim strSQL As String
+    '// Connect MS Access DataBase
+    Function ConnectDataBase() As OleDb.OleDbConnection
+        Return New OleDb.OleDbConnection( _
+            "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & _
+            MyPath(Application.StartupPath) & "data\" & "Countries.accdb;Persist Security Info=True")
+    End Function
+
+    ' / --------------------------------------------------------------------------------
+    ' / Get my project path
+    Function MyPath(ByVal AppPath As String) As String
+        '/ MessageBox.Show(AppPath);
+        MyPath = AppPath.ToLower.Replace("\bin\debug", "\").Replace("\bin\release", "\").Replace("\bin\x86\debug", "\")
+        '/ Return Value
+        '// If not found folder then put the \ (BackSlash) at the end.
+        If Microsoft.VisualBasic.Right(MyPath, 1) <> Chr(92) Then MyPath = MyPath & Chr(92)
+    End Function
+
+    '// Start Here.
+    Private Sub frmXtraGrid_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        DevExpress.Utils.AppearanceObject.DefaultFont = New Font("Tahoma", 10, FontStyle.Regular, GraphicsUnit.Point, 0)
+        Conn = ConnectDataBase()
+        '// Anchor @Runtime
+        Me.GridControl1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right Or AnchorStyles.Top
+        With cmbDisplay
+            .Items.Add("แสดงผลทั้งหมด")
+            .Items.Add("จัดกลุ่มตาม Zone")
+        End With
+        cmbDisplay.SelectedIndex = 0
+        '// จะกระโดดไปเหตุการณ์ SelectedIndexChanged ของ ComboBox
+    End Sub
+
+    Private Sub cmbDisplay_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cmbDisplay.SelectedIndexChanged
+        Select Case cmbDisplay.SelectedIndex
+            Case 0
+                '// Refresh Data.
+                Call GetDataTable(False)
+                Call SetupGridView()
+                Call GroupSumFooter()
+
+            Case 1
+                Call GroupSumFooter()
+                Call GroupByZone()
+        End Select
+    End Sub
+
+    Private Sub SetupGridView()
+        GridView1.Columns.Clear()
+        '// ตั้งค่าคุณสมบัติ XtraGrid
+        '// Start Add Fields.
+        Dim GC As New GridColumn    '// Imports DevExpress.XtraGrid.Columns
+        GC = GridView1.Columns.AddField("CountryPK")
+        With GC
+            .Caption = "CountryPK"
+            .UnboundType = DevExpress.Data.UnboundColumnType.Integer
+            '// ซ่อนหลัก Index = 0 ซึ่งเป็นค่า Primary Key 
+            '// เมื่อผู้ใช้กดดับเบิ้ลคลิ๊กเมาส์ หรือกด Enter ในแต่ละแถว เราจะนำค่านี้ไป Query เพื่อแสดงผลรายละเอียดอีกที
+            .Visible = False
+        End With
+        '// รูปภาพแบบ BLOB (Binary Large OBject) แสดงตัวอย่างในการนำภาพมาแสดงผล
+        GC = GridView1.Columns.AddField("Flag")
+        With GC
+            .Caption = "Flag"
+            .UnboundType = DevExpress.Data.UnboundColumnType.Object
+            .AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center
+            .AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center
+            .Visible = True
+            .Width = 30
+        End With
+        '//
+        GC = GridView1.Columns.AddField("A2")
+        With GC
+            .Caption = "A2"
+            .UnboundType = DevExpress.Data.UnboundColumnType.String
+            .Visible = True
+        End With
+        '//
+        GC = GridView1.Columns.AddField("Country")
+        With GC
+            .Caption = "Country"
+            .UnboundType = DevExpress.Data.UnboundColumnType.String
+            .Visible = True
+        End With
+        '
+        GC = GridView1.Columns.AddField("Capital")
+        With GC
+            .Caption = "Capital"
+            .UnboundType = DevExpress.Data.UnboundColumnType.String
+            .Visible = True
+        End With
+        '//
+        GC = GridView1.Columns.AddField("ZoneName")
+        With GC
+            .Caption = "ZoneName"
+            .UnboundType = DevExpress.Data.UnboundColumnType.String
+            '// หากมีการจัดกลุ่มต้องปิดการแสดงผลหลักนี้
+            If cmbDisplay.SelectedIndex = 0 Then
+                .Visible = True
+            Else
+                .Visible = False
+            End If
+        End With
+        '//
+        GC = GridView1.Columns.AddField("Population")
+        With GC
+            .Caption = "Population"
+            .UnboundType = DevExpress.Data.UnboundColumnType.Decimal
+            .DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+            .DisplayFormat.FormatString = "#,##;(-#,##.00);0"
+            .AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far
+            .AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far
+            .Visible = True
+        End With
+
+        '// Setup XtraGrid Properties @Run Time.
+        Dim view As DevExpress.XtraGrid.Views.Grid.GridView = CType(GridControl1.MainView, DevExpress.XtraGrid.Views.Grid.GridView)
+        DevExpress.XtraEditors.WindowsFormsSettings.DefaultFont = New Font("Tahoma", 10)
+        With view
+            .OptionsBehavior.AutoPopulateColumns = False
+            '// ไม่ให้เกิด GroupPanel เพื่อจับหลักลากมาวางในส่วนนี้ได้ จะใช้การเขียนโค้ดในการจัดกลุ่มแทน
+            .OptionsView.ShowGroupPanel = False
+            .OptionsCustomization.AllowFilter = True '//False
+            .OptionsCustomization.AllowColumnMoving = False
+            '// CheckBox Selector
+            .OptionsSelection.ResetSelectionClickOutsideCheckboxSelector = True
+        End With
+        '//
+        With GridView1
+            .RowHeight = 25
+            .GroupRowHeight = 30
+            .Appearance.Row.Font = New Font("Tahoma", 10, FontStyle.Regular)
+            .Appearance.HeaderPanel.Font = New Font("Tahoma", 10, FontStyle.Bold)
+            .Appearance.GroupRow.Font = New Font("Tahoma", 10, FontStyle.Bold)
+            .Appearance.FocusedRow.Font = New Font("Tahoma", 10, FontStyle.Bold)
+            .Appearance.SelectedRow.Font = New Font("Tahoma", 10)
+            .Appearance.FooterPanel.Font = New Font("Tahoma", 10)
+            ' / Word wrap
+            .OptionsView.RowAutoHeight = True
+            .Appearance.Row.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap
+
+            '// ปรับการเพิ่ม/แก้ไข/ลบข้อมูล
+            .OptionsBehavior.AllowAddRows = False
+            .OptionsBehavior.AllowDeleteRows = False
+            .OptionsBehavior.Editable = False '// True
+            '// Display
+            .FocusRectStyle = DrawFocusRectStyle.RowFocus
+            .Appearance.FocusedCell.ForeColor = Color.Red
+            .Appearance.FocusedCell.Options.UseTextOptions = True
+            .OptionsSelection.EnableAppearanceFocusedCell = False '//True
+            '// Make the group footers always visible.
+            .OptionsView.ShowFooter = True ' False
+            .OptionsView.GroupFooterShowMode = GroupFooterShowMode.VisibleAlways
+        End With
+    End Sub
+
+    '// การค้นหาข้อมูล หรือแสดงผลข้อมูลทั้งหมด จะใช้เพียงโปรแกรมย่อยแบบ Sub Program เดียวกัน
+    '// หากค่าที่ส่งมาเป็น False (หรือไม่ส่งมา จะถือเป็นค่า Default) นั่นคือให้แสดงผลข้อมูลทั้งหมด
+    '// หากค่าที่ส่งมาเป็น True จะเป็นการค้นหาข้อมูล
+    Private Sub GetDataTable(Optional ByVal blnSearch As Boolean = False)
+        strSQL = _
+            " SELECT CountryPK, Flag, A2, Country, Capital, Population, Zones.ZoneName " & _
+            " FROM Countries INNER JOIN Zones ON Countries.ZoneFK = Zones.ZonePK "
+        '// เป็นการค้นหา
+        If blnSearch Then
+            strSQL = strSQL & _
+                    " WHERE " & _
+                    " [A2] " & " Like '%" & Trim(txtSearch.Text) & "%'" & " OR " & _
+                    " [Country] " & " Like '%" & Trim(txtSearch.Text) & "%'" & " OR " & _
+                    " [Capital] " & " Like '%" & Trim(txtSearch.Text) & "%'" & " OR " & _
+                    " [ZoneName] " & " Like '%" & Trim(txtSearch.Text) & "%'"
+            '// Else ไม่ต้องมี
+        End If
+        '// เอา strSQL มาเรียงต่อกัน
+        strSQL = strSQL & " ORDER BY Countries.A2 "
+        '/
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+        Try
+            Cmd = Conn.CreateCommand
+            Cmd.CommandText = strSQL
+            DT = New DataTable
+            DA = New OleDbDataAdapter(Cmd)
+            DA.Fill(DT)
+            '// Bound Data
+            GridControl1.DataSource = DT
+            lblRecordCount.Text = "[จำนวน : " & DT.Rows.Count & " รายการ.]"
+            DT.Dispose()
+            DA.Dispose()
+            txtSearch.Text = ""
+            txtSearch.Focus()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '// Footer for show Sum Total of Population.
+    Sub GroupSumFooter()
+        GridView1.ClearGrouping()
+        GridView1.GroupSummary.Clear()
+        '/ Make sure the group footers always visible.
+        GridView1.OptionsView.ShowFooter = True
+        GridView1.OptionsView.GroupFooterShowMode = GroupFooterShowMode.VisibleAlways
+        '// Creating sum group summaries.
+        Dim SumPopulation As New GridColumn
+        SumPopulation = GridView1.Columns("Population")
+        SumPopulation.SummaryItem.SummaryType = SummaryItemType.Sum
+        '// format the summary of population.
+        SumPopulation.SummaryItem.DisplayFormat = "รวมทั้งหมด : {0:N0}"
+        '// group summary
+        GridView1.GroupSummary.Add(SummaryItemType.Sum, "Population")
+    End Sub
+
+    '// จัดกลุ่มตาม Zone โดยใช้การเขียนโค้ดแทนการจับหลักลากมาวาง
+    Private Sub GroupByZone()
+        GridView1.ClearGrouping()
+        GridView1.GroupSummary.Clear()
+        '/ Import DevExpress.XtraGrid.Views.Grid
+        GridView1.Columns("ZoneName").GroupIndex = 1
+        GridView1.OptionsView.GroupFooterShowMode = GroupFooterShowMode.VisibleIfExpanded
+        '/ Create and setup the Group with ZoneName.
+        Dim ZoneItem As GridGroupSummaryItem = New GridGroupSummaryItem()
+        ZoneItem.FieldName = "ZoneName"
+        '// นับจำนวนประเทศแสดงผลใน Group Header ของแต่ละกลุ่ม
+        ZoneItem.DisplayFormat = "| จำนวน : {0:N0} ประเทศ"
+        ZoneItem.SummaryType = DevExpress.Data.SummaryItemType.Count
+        GridView1.GroupSummary.Add(ZoneItem)
+
+        '/ Create and setup the summary item.
+        Dim SumPopulation As GridGroupSummaryItem = New GridGroupSummaryItem()
+        SumPopulation.FieldName = "Population"
+        SumPopulation.SummaryType = DevExpress.Data.SummaryItemType.Sum
+        SumPopulation.DisplayFormat = "รวม : {0:N0}"
+        SumPopulation.ShowInGroupColumnFooter = GridView1.Columns("Population")
+        GridView1.GroupSummary.Add(SumPopulation)
+    End Sub
+
+    '// แสดงผลทั้งหมด
+    Private Sub btnRefresh_Click(sender As System.Object, e As System.EventArgs) Handles btnRefresh.Click
+        '// Refresh Data.
+        Call GetDataTable()
+        Call SetupGridView()
+        Call GroupSumFooter()
+    End Sub
+
+    '// การค้นหา
+    Private Sub txtSearch_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles txtSearch.KeyPress
+        If Trim(txtSearch.Text.Trim) = "" Or Len(Trim(txtSearch.Text.Trim)) = 0 Then Return
+        '// RetrieveData(True) It means searching for information.
+        If e.KeyChar = Chr(13) Then '// Press Enter
+            '// No beep.
+            e.Handled = True
+            '// Undesirable characters for the database ex.  ', * or %
+            txtSearch.Text = txtSearch.Text.Trim.Replace("'", "").Replace("%", "").Replace("*", "")
+            Call SetupGridView()
+            If cmbDisplay.SelectedIndex = 0 Then
+                Call GetDataTable(True)
+                Call GroupSumFooter()
+            Else
+                Call GetDataTable(True)
+                Call GroupSumFooter()
+                Call GroupByZone()
+            End If
+        End If
+    End Sub
+
+    '// เหตุการณ์ดับเบิ้ลคลิ๊กเมาส์ในแต่ละแถวของตารางกริด
+    Private Sub GridControl1_DoubleClick(sender As Object, e As System.EventArgs) Handles GridControl1.DoubleClick
+        If TryCast(GridControl1.FocusedView, GridView).RowCount = 0 Then Exit Sub
+        '// การรับค่าในแต่ละหลักของตารางกริด
+        Dim PK As Integer = CLng(GridView1.GetRowCellValue(GridView1.GetSelectedRows(0), "CountryPK"))
+        Dim Country As String = GridView1.GetRowCellValue(GridView1.GetSelectedRows(0), "Country")
+        Dim Capital As String = GridView1.GetRowCellValue(GridView1.GetSelectedRows(0), "Capital")
+        'MsgBox(PK)
+        '// หาก PK = 0 จะเป็น Group Header
+        If PK <> 0 Then DevExpress.XtraEditors.XtraMessageBox.Show( _
+            "Primary Key is : " & PK & vbCrLf & _
+            "Country is: " & Country & vbCrLf & _
+            "Capital is: " & Capital _
+            )
+    End Sub
+
+    '// เหตุการณ์กด Enter ในแต่ละแถวของตารางกริด
+    Private Sub GridControl1_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles GridControl1.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Call GridControl1_DoubleClick(sender, e)
+            '// ป้องกันไม่ให้เลื่อนโฟกัสไปแถวถัดไป
+            e.SuppressKeyPress = True
+        End If
+    End Sub
+
+    Private Sub frmXtraGrid_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        If Conn.State = ConnectionState.Open Then Conn.Close()
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+End Class
